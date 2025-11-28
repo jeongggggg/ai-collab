026909aa3bdcfbb3d5fd44a9d6df2ee018ddb9b1
JoAhJeong <ahjeong921@gmail.com>
2025-11-29 03:30:17 +0900

    🧹 Refactor: GithubOAuthProperties Lombok 적용

--- backend/src/main/java/com/aicollab/backend/auth/config/GithubOAuthProperties.java
+++ backend/src/main/java/com/aicollab/backend/auth/config/GithubOAuthProperties.java
@@ -1,35 +1,15 @@
 package com.aicollab.backend.auth.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "github")
+@Getter
+@Setter
 public class GithubOAuthProperties {
 
     private String clientId;
     private String clientSecret;
-    private String redirectIrl;
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
-    }
-
-    public void setClientSecret(String clientSecret) {
-        this.clientSecret = clientSecret;
-    }
-
-    public String getRedirectIrl() {
-        return redirectIrl;
-    }
-
-    public void setRedirectIrl(String redirectIrl) {
-        this.redirectIrl = redirectIrl;
-    }
+    private String redirectUrl;
 }
